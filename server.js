ee11aff3a69b105d1355ae8ca852e286e42ed690
rokzartz <38587835+rokzartz@users.noreply.github.com>
2018-11-03 07:22:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const CLIENT_SECRET = process.env. CLIENT_SECRET; // your bot's client secret
 const redirect = encodeURIComponent('http://agentx.gq:5000/server/callback');
 
 app.get("/", (req, res) => {
-res.render("home");
+res.render("home", { client: clinet});
 })
 
 app.get("/manage/:gld/:user", async (req, res) => {
